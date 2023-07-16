ff5c09fd4d66223f5a039e5a9b3ea2272a47bdce
Deivi Gomez <desgoga@hotmail.com>
2023-07-16 17:43:48 -0500

    primer commit

---
+++ src/main/java/com/devops/demo1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.devops.demo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String index(){
+        return "Hello from Spring Boot!";
+    }
+}
